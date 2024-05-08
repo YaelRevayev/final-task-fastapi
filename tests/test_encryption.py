6da20a501922f34f50516d408d91e7ecfb57868c
YaelRevayev <gexploreapp@gmail.com>
2024-05-08 14:59:23 +0300

    test encryption passed all

--- tests/test_encryption.py
+++ tests/test_encryption.py
@@ -1,25 +1,21 @@
 import unittest
-from unittest.mock import MagicMock, patch, mock_open
+from unittest.mock import MagicMock, patch
 import os
 import sys
+from Crypto.Cipher import AES
 
 project_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir))
-src_dir = os.path.join(project_dir, "src")
 sys.path.append(project_dir)
-sys.path.insert(0, src_dir)
-from src.encryption import read_key_from_file, sign_file
-
-project_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir))
-src_dir = os.path.join(project_dir, "configs")
-sys.path.append(project_dir)
-sys.path.insert(0, src_dir)
+from src.encryption import sign_file, read_key_from_file
 import configs.config as config
 
 
 class TestFileFunctions(unittest.TestCase):
 
     @patch("builtins.open", create=True)
-    def test_key_exact_length(self, mock_open):
+    def test_read_key_from_file_given_exact_length_key_returns_the_same_key(
+        self, mock_open
+    ):
         key_32_bytes = "12345678901234567890123456789012"
         mock_open.return_value.__enter__.return_value.read.return_value = key_32_bytes
 
@@ -29,7 +25,7 @@ class TestFileFunctions(unittest.TestCase):
         self.assertEqual(key, key_32_bytes)
 
     @patch("builtins.open", create=True)
-    def test_key_longer_than_length(self, mock_open):
+    def test_read_key_from_file_given_longer_key_returns_error(self, mock_open):
         mock_open.return_value.__enter__.return_value.read.return_value = (
             b"test_key123456789012345678900000000000000000000000000"
         )
@@ -40,7 +36,7 @@ class TestFileFunctions(unittest.TestCase):
         )
 
     @patch("builtins.open", create=True)
-    def test_key_shorter_than_length(self, mock_open):
+    def test_read_key_from_file_given_shorter_key_returns_error(self, mock_open):
         mock_open.return_value.__enter__.return_value.read.return_value = b"short_key"
         with self.assertRaises(ValueError) as context:
             read_key_from_file("test_key_file")
@@ -50,7 +46,9 @@ class TestFileFunctions(unittest.TestCase):
 
     @patch("hashlib.sha512")
     @patch("Crypto.Cipher.AES.new")
-    def test_sign_file(self, mock_aes_new, mock_sha512):
+    def test_sign_file_given_valid_values_returns_hash_and_iv(
+        self, mock_aes_new, mock_sha512
+    ):
         content = b"test_content"
         key = b"test_key"
 
