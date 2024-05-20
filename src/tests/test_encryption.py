474a32b11dfe78e94f87f79d4bd33d4bb371bd49
YaelRevayev <gexploreapp@gmail.com>
2024-05-20 16:47:46 +0300

    changed tests names

--- src/tests/test_encryption.py
+++ src/tests/test_encryption.py
@@ -11,7 +11,7 @@ import configs as config
 class TestFileFunctions(unittest.TestCase):
 
     @patch("builtins.open", new_callable=mock_open, read_data=b"short_key")
-    def test_key_length_shorter(self, mock_open_file):
+    def test_key_length_given_shorter_key_should_add_empty_spaces(self, mock_open_file):
         key = read_key_from_file("dummy_file_path")
         expected_key = b"short_key" + b" " * (config.AES_KEY_LENGTH - len(b"short_key"))
         self.assertEqual(key, expected_key)
@@ -30,14 +30,16 @@ class TestFileFunctions(unittest.TestCase):
         new_callable=mock_open,
         read_data=b"9a4b5e2c8f1d6a7b3c4e2f9a4b5e2c8f",
     )
-    def test_key_length_exact(self, mock_open_file):
+    def test_key_length_given_exact_key_length_return_the_same_key(
+        self, mock_open_file
+    ):
         key = read_key_from_file("dummy_file_path")
         self.assertEqual(len(key), 32)
         self.assertEqual(key, b"9a4b5e2c8f1d6a7b3c4e2f9a4b5e2c8f")
 
     @patch("hashlib.sha512")
     @patch("Crypto.Cipher.AES.new")
-    def test_sign_file_given_valid_values_returns_hash_and_iv(
+    def test_sign_file_given_valid_values_should_return_hash_and_iv(
         self, mock_aes_new, mock_sha512
     ):
         content = b"test_content"
