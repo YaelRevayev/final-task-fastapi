d7d88d488f27aca48bb1549312bca2e7d337e788
YaelRevayev <gexploreapp@gmail.com>
2024-05-23 17:40:11 +0300

    changed logger

--- src/logger.py
+++ src/logger.py
@@ -1,22 +1,28 @@
 import logging
 from datetime import datetime
+from logging.handlers import TimedRotatingFileHandler
 import os
 
 
-def configure_logger(logger_name: str, handlers: list, log_level=logging.INFO):
+def configure_logger(logger_name: str, log_files: dict):
     logger = logging.getLogger(logger_name)
+    logger.setLevel(logging.DEBUG)
     formatter = logging.Formatter(
         "%(asctime)s - %(filename)s - %(levelname)s - %(message)s"
     )
-    logger.setLevel(log_level)
 
-    for handler_path, level in handlers:
-        # Ensure the directory for log files exists
-        os.makedirs(os.path.dirname(handler_path), exist_ok=True)
-        handler = logging.FileHandler(handler_path)
+    for file_name, level in log_files.items():
+        handler = TimedRotatingFileHandler(
+            file_name, when="midnight", interval=1, backupCount=7
+        )
         handler.setFormatter(formatter)
         handler.setLevel(level)
         logger.addHandler(handler)
+
+    console_handler = logging.StreamHandler()
+    console_handler.setLevel(logging.DEBUG)
+    console_handler.setFormatter(formatter)
+    logger.addHandler(console_handler)
 
     return logger
 
@@ -33,6 +39,10 @@ handlers = [
         logging.INFO,
     ),
     (os.path.join(logs_folder_path, "error_fastapi.log"), logging.ERROR),
+    (
+        os.path.join(logs_folder_path, "debug_fastapi.log"),
+        logging.DEBUG,
+    ),
 ]
 
 fastapi_logger = configure_logger("fastapi_logger", handlers)
